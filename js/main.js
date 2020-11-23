c80e7624a9d10293bc47feb9f267849247c8b8d4
Lucas <lcs.grc@outlook.com>
2020-11-24 00:18:46 +0100

    timer added

--- js/main.js
+++ js/main.js
@@ -4,22 +4,36 @@ import { Grid, gridHTML } from "./class/Grid.js";
 import { timer, resetTimer } from "./timer.js";
 
 const gameMode = document.querySelector("select");
+const restart = document.querySelector(".fa-sync-alt");
 
 // init gameMode
 let game = new Game(gameMode.value);
+startStopTimer(); // TODO : Include this into Game.js
 
 // change gameMode
-gameMode.addEventListener("change", () => {
-  game = new Game(gameMode.value);
-  resetTimer();
-});
+gameMode.addEventListener("change", () => initGame());
+
+// restart game
+restart.addEventListener("click", () => initGame());
 
 // prevent context menu from opening on grid right click
 gridHTML.addEventListener("contextmenu", (e) => e.preventDefault());
 
-Grid.cells().forEach((cellHTML) => {
-  cellHTML.addEventListener("click", () => {
-    if (!game.started) timer();
-    game.started = true;
+// start/stop timer
+// TODO : Include this into Game.js
+function startStopTimer() {
+  Grid.cells().forEach((cellHTML) => {
+    cellHTML.addEventListener("click", () => {
+      if (!game.started) timer();
+      game.started = true;
+    });
   });
-});
+}
+
+// init game
+function initGame() {
+  game = new Game(gameMode.value);
+  resetTimer();
+  game.started = false;
+  startStopTimer();
+}
