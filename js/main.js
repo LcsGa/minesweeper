9533599a8e4ed2d4466e273f22e1531f4d50c62f
Lucas <lcs.grc@outlook.com>
2020-11-25 23:32:21 +0100

    timer : quick fixes + ... functions moved to their right places

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 import "./style/mobile_viewport_height.js";
 import { Game } from "./class/Game.js";
-import { Grid, gridHTML } from "./class/Grid.js";
-import { timer, resetTimer } from "./timer.js";
+import { gridHTML } from "./class/Grid.js";
+import { startTimer, resetTimer } from "./timer.js";
 
 const gameMode = document.querySelector("select");
 const restart = document.querySelector(".fa-sync-alt");
+let game;
 
-// init gameMode
-let game = new Game(gameMode.value);
-startStopTimer(); // TODO : Include this into Game.js
+// init game
+(function initGame() {
+  game = new Game(gameMode.value);
+  resetTimer();
+  game.started = false;
+  startTimer(game);
+})();
 
 // change gameMode
 gameMode.addEventListener("change", () => initGame());
@@ -18,22 +23,3 @@ restart.addEventListener("click", () => initGame());
 
 // prevent context menu from opening on grid right click
 gridHTML.addEventListener("contextmenu", (e) => e.preventDefault());
-
-// start/stop timer
-// TODO : Include this into Game.js
-function startStopTimer() {
-  Grid.cells().forEach((cellHTML) => {
-    cellHTML.addEventListener("click", () => {
-      if (!game.started) timer();
-      game.started = true;
-    });
-  });
-}
-
-// init game
-function initGame() {
-  game = new Game(gameMode.value);
-  resetTimer();
-  game.started = false;
-  startStopTimer();
-}
--- js/timer.js
+++ js/timer.js
@@ -1,11 +1,24 @@
+import { Grid } from "./class/Grid.js";
+
 const timerHTML = document.querySelector("#timer");
 let timerTimeOut;
 let seconds = -1;
 
-export function timer() {
+function timer() {
   seconds++;
   timerHTML.innerHTML = `${seconds < 10 ? "0" : ""}${seconds}`;
   timerTimeOut = setTimeout(timer, 1000);
+}
+
+export function startTimer(gameObj) {
+  Grid.cells().forEach((cellHTML) => {
+    ["click", "contextmenu"].forEach((event) => {
+      cellHTML.addEventListener(event, () => {
+        if (!gameObj.started) timer();
+        gameObj.started = true;
+      });
+    });
+  });
 }
 
 export function resetTimer() {
