c7e22286fae6373fef2a78337513726e67e543c6
Lucas <lcs.grc@outlook.com>
2020-11-29 11:19:08 +0100

    isGameWon() done

--- src/js/classes/Cell.js
+++ src/js/classes/Cell.js
@@ -77,11 +77,11 @@ export class Cell {
       if (gameBombsObj.numberOfBombs === 0) return;
       cellHTML.innerHTML = `<i class="fas fa-flag"></i>`;
       this.cellObj(gameGridObj, cellHTML).hasFlag = true;
-      Bombs.nbOfBombsLeft(gameBombsObj, "reduce");
+      Bombs.nbOfFlagsLeft(gameBombsObj, "reduce");
     } else {
       cellHTML.innerHTML = "";
       this.cellObj(gameGridObj, cellHTML).hasFlag = false;
-      Bombs.nbOfBombsLeft(gameBombsObj, "increase");
+      Bombs.nbOfFlagsLeft(gameBombsObj, "increase");
     }
     window.navigator.vibrate(10);
   }
